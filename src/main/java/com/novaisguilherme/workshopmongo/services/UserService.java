d1d0f786617afb7ac4bc0b08c3f303641e55e237
guiga18br <guilhermenovaislimaa@hotmail.com>
2022-04-16 16:48:04 -0300

    Post Insert User

--- src/main/java/com/novaisguilherme/workshopmongo/services/UserService.java
+++ src/main/java/com/novaisguilherme/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.novaisguilherme.workshopmongo.domain.User;
+import com.novaisguilherme.workshopmongo.dto.UserDTO;
 import com.novaisguilherme.workshopmongo.repository.UserReposityory;
 import com.novaisguilherme.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User Insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
